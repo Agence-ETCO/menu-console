e0c2ced9672a5533a3feccd413397e6bfa664f43
Yanick Landry <yanick.landry@gmail.com>
2022-08-10 19:15:40 -0400

    fixed build

--- pages/6/index.js
+++ pages/6/index.js
@@ -13,7 +13,7 @@ import {
 
 const Page6 = () => {
   const date = "le 30 septembre 2022";
-  const user = JSON.parse(localStorage.getItem("user") || '{}');
+  const user = typeof window !== "undefined" && JSON.parse(localStorage.getItem("user") || '{}');
   return (
     <>
       <Header />
