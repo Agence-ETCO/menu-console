f730d3373a98a2ac22e9c8193b981ce5810bd980
Yanick Landry <yanick.landry@gmail.com>
2022-09-04 14:15:21 -0400

    refactored api call and login

--- pages/login/index.js
+++ pages/login/index.js
@@ -13,53 +13,43 @@ const Page = () => {
   const router = useRouter();
   const { loginToken } = router.query;
 
-  useEffect(() => {
-    if (loginToken) {
-      login(loginToken)
-        .then(({ data }) => {
-          const { jwt, user } = data;
-          /*    const jwt =
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYxODgxNTI0LCJleHAiOjE2NjE5Njc5MjR9.AqIFRJxaQTb_mZgFoBwvKyFjOhPKKZPrSDcYjy4P9ao";
-          const user = {
-            id: 2,
-          }; */
-          localStorage.setItem("jwt", jwt);
-          localStorage.setItem("user", JSON.stringify(user));
+  const doLogin = async () => {
+    const { data } = await login(loginToken);
+    const { jwt, user } = data;
+    /*    const jwt =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNjYxODgxNTI0LCJleHAiOjE2NjE5Njc5MjR9.AqIFRJxaQTb_mZgFoBwvKyFjOhPKKZPrSDcYjy4P9ao";
+    const user = {
+      id: 2,
+    }; */
+    localStorage.setItem('jwt', jwt);
+    localStorage.setItem('user', JSON.stringify(user));
 
-          return fetchAPI(`/api/users/${user.id}?populate=deep`, jwt).then(
-            (res) => {
-              if (res.franchisee_s_menu) {
-                if (res.franchisee_s_menu.id) {
-                  getMenuId(res.franchisee_s_menu.id);
-                }
-              } else {
-                const data = {
-                  menu_items: [],
-                  craftOptions: {},
-                  franchisee: user.id,
-                };
+    const res = await fetchAPI(`/api/users/${user.id}?populate=deep`, jwt);
+    if (res.franchisee_s_menu) {
+      if (res.franchisee_s_menu.id) {
+        getMenuId(res.franchisee_s_menu.id);
+      }
+    } else {
+      const data = {
+        menu_items: [],
+        craftOptions: {},
+        franchisee: user.id,
+      };
 
-                return postAPI("api/franchisees-menus", jwt, data)
-                  .then((res) => {
-                    getMenuId(res.data.id);
-                    console.log(res.data.id);
-                  })
-                  .catch((err) => {
-                    console.log(err);
-                  });
-              }
-              router.push("/");
-            }
-          );
-        })
-
-        // setDisplay(`Successfully logged in as ${JSON.stringify(user, null, 2)}`);
-
-        .catch((err) => {
-          console.log(err);
-          setDisplay("Error with token");
-        });
+      const resMenu = await postAPI('api/franchisees-menus', jwt, data)
+      getMenuId(resMenu.data.id);
+      console.log(resMenu.data.id);
     }
+    router.push("/");
+  }
+
+  useEffect(() => {
+    if (!(router.isReady && loginToken && getMenuId)) return;
+    doLogin().catch(err => {
+      console.error(err);
+      setDisplay('Error with token');
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router, loginToken]);
 
   return <p>{display}</p>;
