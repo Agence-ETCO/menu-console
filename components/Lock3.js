6ce83e1b04c13bf36380eae5f00ac90dfa7eed83
sandra-etco <110406772+sandra-etco@users.noreply.github.com>
2022-09-05 17:14:04 -0400

    update

---
+++ components/Lock3.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const Lock3 = (props) => (
+  <svg
+    width={27}
+    height={34}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M22.92 12.67h-.83v-4C22.08 3.89 18.2 0 13.42 0 8.64 0 4.75 3.89 4.75 8.67v4h-.83C1.76 12.67 0 14.43 0 16.59v12.67c0 2.16 1.76 3.92 3.92 3.92h19c2.16 0 3.92-1.76 3.92-3.92V16.59c0-2.16-1.76-3.92-3.92-3.92Zm-13.5 0v-4c0-2.21 1.79-4 4-4s4 1.79 4 4v4h-8ZM22.17 28.5H4.67V17.33h17.5V28.5Z"
+      fill="#E31E29"
+    />
+  </svg>
+);
+
+export default Lock3;
