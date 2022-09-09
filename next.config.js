804bbc352c6be1e5ad5f018676d8909753effeb0
Yanick Landry <yanick.landry@gmail.com>
2022-09-09 11:30:14 -0400

    SH108485-8 added export pages

--- next.config.js
+++ next.config.js
@@ -11,7 +11,18 @@ const nextConfig = {
   trailingSlash: true,
   exportPathMap: async function () {
     const paths = {
-      '/': { page: '/' }
+      '/': { page: '/' },
+      '/1': { page: '/1' },
+      '/2': { page: '/2' },
+      '/3': { page: '/3' },
+      '/4': { page: '/4' },
+      '/5': { page: '/5' },
+      '/6': { page: '/6' },
+      '/7': { page: '/7' },
+      '/8': { page: '/8' },
+      '/login': { page: '/login' },
+      '/WelcomePage': { page: '/WelcomePage' },
+      '/login-test': { page: '/login-test' },
     };
     return paths;
   },
