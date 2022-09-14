4912945da941dd085fc8ba5e4002e9bbdbc7e9c1
sandra-etco <110406772+sandra-etco@users.noreply.github.com>
2022-09-14 19:49:40 -0400

    fix

--- pages/7/index.js
+++ pages/7/index.js
@@ -14,6 +14,7 @@ import {
 } from "./styled";
 
 const Page7 = () => {
+  const router = useRouter();
   useEffect(() => {
     if (typeof window !== "undefined" && localStorage.getItem("isSubmitted")) {
       router.push("/9");
